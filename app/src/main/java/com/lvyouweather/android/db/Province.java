05e992a9b3a37be098c76ba8dc9f7bd5fe6546c4
tangjianfeng <394752787@qq.com>
2017-04-11 20:58:09 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/lvyouweather/android/db/Province.java
@@ -0,0 +1,41 @@
+package com.lvyouweather.android.db;
+
+
+/**
+ * Created by 唐健峰 on 2017/4/10.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
